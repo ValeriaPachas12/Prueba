9e9ea905890e6fa0af5f7795afada7210ea11081
Valeria <valeleriapa226@gmail.com>
2024-06-07 16:28:11 -0500

    carga inicial

---
+++ REPASO-2.sql
@@ -0,0 +1,10 @@
+SELECT TO_DATE ('27/05/2024', 'DD/MM/YYYY')AS PRROS_TODOS
+FROM dual;
+
+SELECT TO_NUMBER('3654,25') AS uwu_numeritokawai
+FROM dual;
+
+SELECT CONCAT ('hola ', 'prre') as tablita from dual;
+
+SELECT CONCAT (nombre_empleado, apellido_materno, apellido_paterno )as aea 
+from empleado;
